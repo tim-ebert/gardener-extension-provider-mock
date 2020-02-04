b161a2a13c543722b81b0f5f51baf2cf28008ea9
Tim Ebert <tim.ebert@sap.com>
2020-02-04 09:21:23 +0100

    Also clean ControlPlane controller from aws values

--- pkg/controller/controlplane/valuesprovider.go
+++ pkg/controller/controlplane/valuesprovider.go
@@ -16,15 +16,11 @@ package controlplane
 
 import (
 	"context"
-	"path/filepath"
 
-	apisaws "github.com/gardener/gardener-extensions/controllers/provider-aws/pkg/apis/aws"
-	"github.com/gardener/gardener-extensions/controllers/provider-aws/pkg/apis/aws/helper"
-	"github.com/gardener/gardener-extensions/controllers/provider-aws/pkg/aws"
+	apismock "github.com/gardener/gardener-extension-provider-mock/pkg/apis/mock"
 	"github.com/gardener/gardener-extensions/pkg/controller"
 	extensionscontroller "github.com/gardener/gardener-extensions/pkg/controller"
 	"github.com/gardener/gardener-extensions/pkg/controller/common"
-	"github.com/gardener/gardener-extensions/pkg/controller/controlplane"
 	"github.com/gardener/gardener-extensions/pkg/controller/controlplane/genericactuator"
 	"github.com/gardener/gardener-extensions/pkg/util"
 
@@ -35,15 +31,6 @@ import (
 	"github.com/gardener/gardener/pkg/utils/secrets"
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	"k8s.io/apiserver/pkg/authentication/user"
-)
-
-// Object names
-const (
-	cloudControllerManagerServerName = "cloud-controller-manager-server"
 )
 
 var controlPlaneSecrets = &secrets.Secrets{
@@ -55,30 +42,7 @@ var controlPlaneSecrets = &secrets.Secrets{
 		},
 	},
 	SecretConfigsFunc: func(cas map[string]*secrets.Certificate, clusterName string) []secrets.ConfigInterface {
-		return []secrets.ConfigInterface{
-			&secrets.ControlPlaneSecretConfig{
-				CertificateSecretConfig: &secrets.CertificateSecretConfig{
-					Name:         aws.CloudControllerManagerName,
-					CommonName:   "system:cloud-controller-manager",
-					Organization: []string{user.SystemPrivilegedGroup},
-					CertType:     secrets.ClientCert,
-					SigningCA:    cas[v1beta1constants.SecretNameCACluster],
-				},
-				KubeConfigRequest: &secrets.KubeConfigRequest{
-					ClusterName:  clusterName,
-					APIServerURL: v1beta1constants.DeploymentNameKubeAPIServer,
-				},
-			},
-			&secrets.ControlPlaneSecretConfig{
-				CertificateSecretConfig: &secrets.CertificateSecretConfig{
-					Name:       cloudControllerManagerServerName,
-					CommonName: aws.CloudControllerManagerName,
-					DNSNames:   controlplane.DNSNamesForService(aws.CloudControllerManagerName, clusterName),
-					CertType:   secrets.ServerCert,
-					SigningCA:  cas[v1beta1constants.SecretNameCACluster],
-				},
-			},
-		}
+		return []secrets.ConfigInterface{}
 	},
 }
 
@@ -91,81 +55,28 @@ var controlPlaneExposureSecrets = &secrets.Secrets{
 		},
 	},
 	SecretConfigsFunc: func(cas map[string]*secrets.Certificate, clusterName string) []secrets.ConfigInterface {
-		return []secrets.ConfigInterface{
-			&secrets.ControlPlaneSecretConfig{
-				CertificateSecretConfig: &secrets.CertificateSecretConfig{
-					Name:         aws.LBReadvertiserDeploymentName,
-					CommonName:   aws.LBReadvertiserDeploymentName,
-					Organization: []string{user.SystemPrivilegedGroup},
-					DNSNames:     nil,
-					IPAddresses:  nil,
-					CertType:     secrets.ClientCert,
-					SigningCA:    cas[v1beta1constants.SecretNameCACluster],
-				},
-
-				KubeConfigRequest: &secrets.KubeConfigRequest{
-					ClusterName:  clusterName,
-					APIServerURL: v1beta1constants.DeploymentNameKubeAPIServer,
-				},
-			},
-		}
+		return []secrets.ConfigInterface{}
 	},
 }
 
-var configChart = &chart.Chart{
-	Name: "cloud-provider-config",
-	Path: filepath.Join(aws.InternalChartsPath, "cloud-provider-config"),
-	Objects: []*chart.Object{
-		{
-			Type: &corev1.ConfigMap{},
-			Name: aws.CloudProviderConfigName,
-		},
-	},
-}
+var configChart *chart.Chart = nil
 
-var ccmChart = &chart.Chart{
-	Name:   "cloud-controller-manager",
-	Path:   filepath.Join(aws.InternalChartsPath, "cloud-controller-manager"),
-	Images: []string{aws.CloudControllerManagerImageName},
-	Objects: []*chart.Object{
-		{Type: &corev1.Service{}, Name: "cloud-controller-manager"},
-		{Type: &appsv1.Deployment{}, Name: "cloud-controller-manager"},
-		{Type: &corev1.ConfigMap{}, Name: "cloud-controller-manager-monitoring-config"},
-	},
-}
+var ccmChart *chart.Chart = nil
 
-var ccmShootChart = &chart.Chart{
-	Name: "cloud-controller-manager-shoot",
-	Path: filepath.Join(aws.InternalChartsPath, "cloud-controller-manager-shoot"),
-	Objects: []*chart.Object{
-		{Type: &rbacv1.ClusterRole{}, Name: "system:controller:cloud-node-controller"},
-		{Type: &rbacv1.ClusterRoleBinding{}, Name: "system:controller:cloud-node-controller"},
-	},
-}
+var ccmShootChart *chart.Chart = nil
 
-var storageClassChart = &chart.Chart{
-	Name: "shoot-storageclasses",
-	Path: filepath.Join(aws.InternalChartsPath, "shoot-storageclasses"),
-}
+var storageClassChart *chart.Chart = nil
 
-var cpExposureChart = &chart.Chart{
-	Name:   "aws-lb-readvertiser",
-	Path:   filepath.Join(aws.InternalChartsPath, "aws-lb-readvertiser"),
-	Images: []string{aws.AWSLBReadvertiserImageName},
-	Objects: []*chart.Object{
-		{Type: &appsv1.Deployment{}, Name: "aws-lb-readvertiser"},
-		{Type: extensionscontroller.GetVerticalPodAutoscalerObject(), Name: "aws-lb-readvertiser-vpa"},
-	},
-}
+var cpExposureChart *chart.Chart = nil
 
 // NewValuesProvider creates a new ValuesProvider for the generic actuator.
 func NewValuesProvider(logger logr.Logger) genericactuator.ValuesProvider {
 	return &valuesProvider{
-		logger: logger.WithName("aws-values-provider"),
+		logger: logger.WithName("mock-values-provider"),
 	}
 }
 
-// valuesProvider is a ValuesProvider that provides AWS-specific values for the 2 charts applied by the generic actuator.
+// valuesProvider is a ValuesProvider that provides Mock-specific values for the 2 charts applied by the generic actuator.
 type valuesProvider struct {
 	genericactuator.NoopValuesProvider
 	common.ClientContext
@@ -179,7 +90,7 @@ func (vp *valuesProvider) GetConfigChartValues(
 	cluster *extensionscontroller.Cluster,
 ) (map[string]interface{}, error) {
 	// Decode infrastructureProviderStatus
-	infraStatus := &apisaws.InfrastructureStatus{}
+	infraStatus := &apismock.InfrastructureStatus{}
 	if cp.Spec.InfrastructureProviderStatus != nil {
 		if _, _, err := vp.Decoder().Decode(cp.Spec.InfrastructureProviderStatus.Raw, nil, infraStatus); err != nil {
 			return nil, errors.Wrapf(err, "could not decode infrastructureProviderStatus of controlplane '%s'", util.ObjectName(cp))
@@ -199,7 +110,7 @@ func (vp *valuesProvider) GetControlPlaneChartValues(
 	scaledDown bool,
 ) (map[string]interface{}, error) {
 	// Decode providerConfig
-	cpConfig := &apisaws.ControlPlaneConfig{}
+	cpConfig := &apismock.ControlPlaneConfig{}
 	if cp.Spec.ProviderConfig != nil {
 		if _, _, err := vp.Decoder().Decode(cp.Spec.ProviderConfig.Raw, nil, cpConfig); err != nil {
 			return nil, errors.Wrapf(err, "could not decode providerConfig of controlplane '%s'", util.ObjectName(cp))
@@ -210,7 +121,7 @@ func (vp *valuesProvider) GetControlPlaneChartValues(
 	return getCCMChartValues(cpConfig, cp, cluster, checksums, scaledDown)
 }
 
-// GetControlPlaneExposureChartValues deploys the aws-lb-readvertiser.
+// GetControlPlaneExposureChartValues returns the control plane exposure values
 func (vp *valuesProvider) GetControlPlaneExposureChartValues(
 	ctx context.Context,
 	cp *extensionsv1alpha1.ControlPlane,
@@ -229,37 +140,26 @@ func (vp *valuesProvider) GetControlPlaneExposureChartValues(
 	}
 
 	return map[string]interface{}{
-		"domain":   address,
-		"replicas": extensionscontroller.GetReplicas(cluster, 1),
-		"podAnnotations": map[string]interface{}{
-			"checksum/secret-aws-lb-readvertiser": checksums[aws.LBReadvertiserDeploymentName],
-		},
+		"domain":         address,
+		"replicas":       extensionscontroller.GetReplicas(cluster, 1),
+		"podAnnotations": map[string]interface{}{},
 	}, nil
 }
 
 // getConfigChartValues collects and returns the configuration chart values.
 func getConfigChartValues(
-	infraStatus *apisaws.InfrastructureStatus,
+	infraStatus *apismock.InfrastructureStatus,
 	cp *extensionsv1alpha1.ControlPlane,
 ) (map[string]interface{}, error) {
-	// Get the first subnet with purpose "public"
-	subnet, err := helper.FindSubnetForPurpose(infraStatus.VPC.Subnets, apisaws.PurposePublic)
-	if err != nil {
-		return nil, errors.Wrapf(err, "could not determine subnet from infrastructureProviderStatus of controlplane '%s'", util.ObjectName(cp))
-	}
-
 	// Collect config chart values
 	return map[string]interface{}{
-		"vpcID":       infraStatus.VPC.ID,
-		"subnetID":    subnet.ID,
 		"clusterName": cp.Namespace,
-		"zone":        subnet.Zone,
 	}, nil
 }
 
 // getCCMChartValues collects and returns the CCM chart values.
 func getCCMChartValues(
-	cpConfig *apisaws.ControlPlaneConfig,
+	cpConfig *apismock.ControlPlaneConfig,
 	cp *extensionsv1alpha1.ControlPlane,
 	cluster *extensionscontroller.Cluster,
 	checksums map[string]string,
@@ -270,12 +170,7 @@ func getCCMChartValues(
 		"clusterName":       cp.Namespace,
 		"kubernetesVersion": cluster.Shoot.Spec.Kubernetes.Version,
 		"podNetwork":        extensionscontroller.GetPodNetwork(cluster),
-		"podAnnotations": map[string]interface{}{
-			"checksum/secret-cloud-controller-manager":        checksums[aws.CloudControllerManagerName],
-			"checksum/secret-cloud-controller-manager-server": checksums[cloudControllerManagerServerName],
-			"checksum/secret-cloudprovider":                   checksums[v1beta1constants.SecretNameCloudProvider],
-			"checksum/configmap-cloud-provider-config":        checksums[aws.CloudProviderConfigName],
-		},
+		"podAnnotations":    map[string]interface{}{},
 	}
 
 	if cpConfig.CloudControllerManager != nil {
